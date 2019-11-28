f0c5b75992e122be03780c651f9c5257a6006b60
luís fernando <munhozfontana@gmail.com>
2019-11-27 22:50:10 -0300

    correção pilha snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -131,6 +131,7 @@ class _HomeState extends State<Home> {
                 }),
             duration: Duration(seconds: 4),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
